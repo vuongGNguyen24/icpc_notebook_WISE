4e49129ae2ea46c00006bc55d1950749a4120d50
vuongGNguyen24 <vuongvietashd@gmail.com>
2024-08-23 10:04:58 +0700

    Various change for our notebook

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,14 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define FOR(i, a, b) for (int i = (a), _b = (b); i <= _b; ++i)
+#define PR(a,n) { cerr << #a << " = "; FOR(_,1,n) cerr << a[_] << ' '; cerr << endl; }
+#define PR0(a,n) { cerr << #a << " = "; FOR(_, 0, n - 1) cerr << a[_] << ' '; cerr << endl; }
+#define debug(x) cerr << #x << " = " << x << endl
+#define TIME (1.0 * clock() / CLOCKS_PER_SEC)
+#define __builtin_popcount __builtin_popcountll
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+template <class T, class T2> ostream &operator<<(ostream &o, pair<T, T2> p)
+{
+    o << "(" << p.first << ", " << p.second << ")"; 
+    return o;
+}
 
-#define rep(i, a, b) for(int i = a; i < (b); ++i)
-#define all(x) begin(x), end(x)
-#define sz(x) (int)(x).size()
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef vector<int> vi;
+void solve()
+{
 
-int main() {
-	cin.tie(0)->sync_with_stdio(0);
-	cin.exceptions(cin.failbit);
+}
+
+signed main() 
+{
+    #ifdef LOCAL
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+        freopen("log.txt", "w", stderr);
+    #endif
+    ios_base::sync_with_stdio(0);
+	cin.tie(0); 
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
 }
--- content/data-structures/OrderStatisticTree.h
+++ content/data-structures/OrderStatisticTree.h
@@ -11,10 +11,12 @@
 #pragma once
 
 #include <bits/extc++.h> /** keep-include */
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
 
 template<class T>
-using Tree = tree<T, null_type, less<T>, rb_tree_tag,
+using Tree = tree<T, null_type, std::less<T>, rb_tree_tag,
     tree_order_statistics_node_update>;
 
 void example() {
@@ -24,5 +26,4 @@ void example() {
 	assert(t.order_of_key(10) == 1);
 	assert(t.order_of_key(11) == 2);
 	assert(*t.find_by_order(0) == 8);
-	t.join(t2); // assuming T < T2 or T > T2, merge t2 into t
 }
